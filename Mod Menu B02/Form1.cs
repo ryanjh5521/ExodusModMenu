078629f154dde2ef43a8cb90ba6403a9e6020c19
ryanjh5521 <ryanjh5521@gmail.com>
2013-08-27 15:59:04 -0400

    first commit

---
+++ Mod Menu B02/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using XRPCLib;
+using System.Threading;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        XRPC Jtag = new XRPC();
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Jtag.Connect();
+
+
+            if (Jtag.activeConnection)
+            {
+                System.Windows.Forms.MessageBox.Show("Connected");
+
+
+            }
+            else
+            {
+                System.Windows.Forms.MessageBox.Show("Connection Failed");
+            }
+        }
+    }
+}
